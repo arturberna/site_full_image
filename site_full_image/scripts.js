578642131a44a1938cf569a1fc9a6e898f56dc34
arturberna <67850791+arturberna@users.noreply.github.com>
2022-06-15 10:53:46 -0300

    Add files via upload

---
+++ site_full_image/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  $('.header').height($(window).height());
+})
+
+function verifyDate(){
+	var origem = document.getElementById('ida');
+  // new Date()
+	alert(origem.value);
+}
